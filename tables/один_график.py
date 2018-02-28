960f05abfd7f9ae2c95b12fb9f764f74de11aa93
Fedor Sarafanov <sfg180@yandex.ru>
2018-02-28 20:59:43 +0300

    Auto stash before merge of "master" and "origin/Наборметодички"

--- tables/один_график.py
+++ tables/один_график.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy import pi,sqrt, linspace
 import matplotlib.pyplot as plt
+from random import uniform as uniform
 
 def processing(name,saveto, om):
 	plt.cla()
@@ -20,15 +21,23 @@ def processing(name,saveto, om):
 	Q=np.max(uc)/e0
 	nu0=nu[np.argmax(ur)]
 	w0=nu0*2*pi
-
-	print('Q=',Q)
-	print('nu0=',nu0)
-	print('w0=',w0)
-
 	C=6.8e-09
 	R=1/C/w0/Q
 	L=1/C/w0**2
 
+	Rp=float(om)
+	Lp=0.708
+	Cp=6.8e-09
+
+	print('Q сравнением (по нему график)=',Q)
+	if Rp==2400:
+		print('Q методом ширины (2400)=',w0/abs(12379-16181))
+	else:
+		print('Q методом ширины (100)=',w0/abs(14211-14919))
+	print('Q расчетом=',1/Rp*sqrt(Lp/Cp))
+
+	print('nu0=',nu0)
+	print('w0=',w0)
 	print('e0=',e0,'<=из эксперимента')
 	print('C=',C,'<=считаем известным')
 	print('R=',R,'<=предполагалось',om)
@@ -36,15 +45,27 @@ def processing(name,saveto, om):
 
 	w_th=linspace(1,20000,500)
 
-	Q=np.max(uc)/e0*0.992
-	uc_th=e0*w0**2/sqrt(w0**2*w_th**2/Q**2+(w_th**2-w0**2)**2)
+	# Q=np.max(uc)/e0*0.992
 	Q=np.max(uc)/e0
+
+	# def uct(w):
+		# return e0*w0**2/sqrt(w0**2*w**2/Q**2+(w**2-w0**2)**2)
+
+	uc_th=e0*w0**2/sqrt(w0**2*w_th**2/Q**2+(w_th**2-w0**2)**2)
 	ul_th=e0*w_th**2/sqrt(w0**2*w_th**2/Q**2+(w_th**2-w0**2)**2)
 	ur_th=e0*w0*w_th/sqrt(w0**2*w_th**2+Q**2*(w_th**2-w0**2)**2)*0.91
 
+	# for i in range(0,len(w)):
+		# d=0.001
+		# uniform(a,b)
+	u_eff=np.max(uc)/sqrt(2)
+	plt.axhline(y=u_eff, color='r', linestyle='-',linewidth=0.5)
+	#здесь получена \Delta\omega:
+	# print('\Delta\omega=',abs(12379-16181))
 	plt.plot(w,uc,'r.')
 	plt.plot(w,ul,'g.')
 	plt.plot(w,ur,'b.')
+	# plt.plot(w,ur*2.5,'m.')
 
 	plt.plot(w_th,uc_th,'r',label=u'$U_C$')
 	plt.plot(w_th,ul_th,'g',label=u'$U_L$')
@@ -66,4 +87,5 @@ def processing(name,saveto, om):
 	plt.show()
 
 processing('r2400_0.2541.tsv','../plot/r2400.pdf','2400')
-# processing('r100.tsv','../plot/r100.pdf','100')
+print('----------------')
+processing('r100.tsv','../plot/r100.pdf','100')
